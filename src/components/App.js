26c72b6ab5d1d95e43c6d484b341e2d35cffeef5
Tina Shi <init.defaultBranch>
2023-04-24 17:26:49 +1000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [image, setImage] = useState(null)
+    
+    useEffect (() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((res) => res.json())
+            .then((data) => setImage(data.message))
+    }, [])
+
+    if (!image) {
+        return <p>"Loading..."</p>
+    } else {
+        return (
+            <img alt="A Random Dog" src={image}></img>
+        )
+    }
+}
+
+export default App;
